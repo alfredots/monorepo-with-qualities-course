12b52369c9dd37b518fa9838d34b7014decb226b
alfredo tito <tito.alfred@hotmail.com>
2025-05-12 19:01:54 -0300

    A1.3: Criando pacote http client na commons

--- apps/company-front/app/page.tsx
+++ apps/company-front/app/page.tsx
@@ -1,3 +1,11 @@
+"use client";
+import { httpClient } from '@alfredots/commons-http-client'
+
 export default function Page() {
-    return <h1>Hello, World!</h1>
-  }
+  httpClient
+    .get("https://api.github.com/users/alfredots")
+    .then(data => console.log(data))
+
+
+  return <h1>Hello, World!</h1>
+}

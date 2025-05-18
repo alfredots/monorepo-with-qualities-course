d7b256b07381d495fac863e6614cf317085c65c9
alfredo tito <tito.alfred@hotmail.com>
2025-05-18 00:39:36 -0300

    A2.4: Adicionado lint-staged

--- apps/company-front/app/page.tsx
+++ apps/company-front/app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 import { httpClient } from '@alfredots/commons-http-client'
 
+
 export default function Page() {
   httpClient
     .get("https://api.github.com/users/alfredots")
     .then(data => console.warn(data))
-
 
   return <h1>Hello, World!</h1>
 }
